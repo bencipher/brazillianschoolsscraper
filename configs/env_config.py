b115672b7a893146ae20e6de954861ed772af648
bcx <femolak@gmail.com>
2024-06-23 19:29:21 +0200

    remove dotenv dependency on all files

---
+++ configs/env_config.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+import os
+
+
+class EnvironmentConfig:
+    def __init__(self, dotenv_path):
+        self.dotenv_path = dotenv_path
+        self.loaded = False
+
+    def load_env(self):
+        if not self.loaded:
+            load_dotenv(self.dotenv_path)
+            self.loaded = True
+
+    def get_env_variable(self, key):
+        self.load_env()
+        return os.getenv(key)
